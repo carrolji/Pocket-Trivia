5a5f4b5b66183db3d5542e7de8d87c2294d92ba6
carrolji <jirakul@ualberta.ca>
2017-07-17 23:27:03 -0700

    welcome page

--- part2/main.cpp
+++ part2/main.cpp
@@ -5,12 +5,26 @@
 #include<stdlib.h>
 using namespace std;
 
+void start()
+{
+     /* stuff to clear */
+     
+     textout_ex(screen, font, "Pick the type of quizes:", 5, 10, makecol(1, 1, 255), makecol(150, 1,1));
+     textout_ex(screen, font, "Enter (1) for entire text:", 5, 20, makecol(1, 1, 255), makecol(150,1,1));
+     textout_ex(screen, font, "Enter (2) for particular unit:", 5, 30, makecol(1, 1, 255), makecol(150,1,1));
+     textout_ex(screen, font, "Enter (3) for particular chapter:", 5, 40, makecol(1, 1, 255), makecol(150,1,1));
+     
+ }
+ 
+ 
 int main(void) {
 	allegro_init(); 
 	set_gfx_mode(GFX_SAFE, 640, 480, 0, 0); 
 	install_keyboard(); 
 	textout_ex(screen, font, "Welcome to Pocket Trivia Game", 1, 1, 10, -1); 
-	textout_ex(screen, font, "Press ESCape to quit.", 1, 12, 11, -1); 
+	start();
+    release_screen();
+	
 	
 	string line;
 	ifstream myfile ("test.txt");
@@ -18,7 +32,7 @@ int main(void) {
 		while(!myfile.eof()){
 			getline (myfile, line);
 			cout << line << '\n';
-			textprintf_ex(screen, font, 0,24, 15,-1, "%s\n", line.c_str());
+			//textprintf_ex(screen, font, 0,24, 15,-1, "%s\n", line.c_str());
 		}
 		myfile.close();
 	}
