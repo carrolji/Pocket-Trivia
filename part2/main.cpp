57a7ded1402afcc77aa8ebe4531aae52554d6e0e
carrolji <jirakul@ualberta.ca>
2017-07-17 23:06:18 -0700

    testing file

--- part2/main.cpp
+++ part2/main.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<stdlib.h>
 using namespace std;
 
 int main(void) {
@@ -12,19 +13,21 @@ int main(void) {
 	textout_ex(screen, font, "Press ESCape to quit.", 1, 12, 11, -1); 
 	
 	string line;
-	ifstream myfile ("chp1.txt");
+	ifstream myfile ("test.txt");
 	if (myfile.is_open()){
 		while(!myfile.eof()){
 			getline (myfile, line);
 			cout << line << '\n';
-			textprintf_ex(screen, font, 0,300, makecol(255,255,255),-1, "line: %s", line.c_str());
-			//textout_ex(screen, font, line, 1, 1, 10, -1); 
+			textprintf_ex(screen, font, 0,24, 15,-1, "%s\n", line.c_str());
 		}
 		myfile.close();
 	}
 	else cout << "Unable to open file";
 	
-	while(!key[KEY_ESC]); 
+	while(!key[KEY_ESC]){
+		
+		circle(screen,300,300,50,makecol(255,255,255));
+	}; 
 	allegro_exit(); 
 	return 0;
 } 
