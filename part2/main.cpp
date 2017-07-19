2645ab69516e53b737e4efb7ae1c86aece84477d
carrolji <jirakul@ualberta.ca>
2017-07-18 23:47:36 -0700

    install keyboard

--- part2/main.cpp
+++ part2/main.cpp
@@ -5,25 +5,25 @@
 #include<stdlib.h>
 using namespace std;
 
-void start()
-{
-     /* stuff to clear */
-     textout_ex(screen, font, "Welcome to Pocket Trivia Game", 5, 1, 10, -1); 
-     textout_ex(screen, font, "Pick the type of quizes:", 5, 10, makecol(1, 1, 255), makecol(150, 1,1));
-     textout_ex(screen, font, "Enter (1) for entire text:", 5, 20, makecol(1, 1, 255), makecol(150,1,1));
-     textout_ex(screen, font, "Enter (2) for particular unit:", 5, 30, makecol(1, 1, 255), makecol(150,1,1));
-     textout_ex(screen, font, "Enter (3) for particular chapter:", 5, 40, makecol(1, 1, 255), makecol(150,1,1));
-     
- }
- 
- 
+void start();
+void clear_screen();
+void get_input();
+
+//BITMAP *buffer = create_bitmap(640, 480);
 int main(void) {
-	allegro_init(); 
-	set_gfx_mode(GFX_SAFE, 640, 480, 0, 0); 
-	install_keyboard(); 
+	int ret;
+	allegro_init();  
+	install_keyboard();
+	install_timer();
+	ret = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
+	if(ret != 0){
+		allegro_message(allegro_error);
+		return 1;
+	} 
 	start();
-    release_screen();
-	
+	if(key[KEY_1]){
+			circle(screen,300,300,50,15);
+	}
 	
 	string line;
 	ifstream myfile ("test.txt");
@@ -39,9 +39,26 @@ int main(void) {
 	
 	while(!key[KEY_ESC]){
 		
-		circle(screen,300,300,50,makecol(255,255,255));
+		
+		//rectfill(screen,0,0,640,480,15);
 	}; 
 	allegro_exit(); 
 	return 0;
 } 
 END_OF_MAIN()
+
+
+void start()
+{
+     /* stuff to clear */
+     textout_ex(screen, font, "Welcome to Pocket Trivia Game", 5, 1, 10, -1); 
+     textout_ex(screen, font, "Pick the type of quizes:", 5, 10, makecol(1, 1, 255), makecol(150, 1,1));
+     textout_ex(screen, font, "Enter (1) for entire text:", 5, 20, makecol(1, 1, 255), makecol(150,1,1));
+     textout_ex(screen, font, "Enter (2) for particular unit:", 5, 30, makecol(1, 1, 255), makecol(150,1,1));
+     textout_ex(screen, font, "Enter (3) for particular chapter:", 5, 40, makecol(1, 1, 255), makecol(150,1,1));
+     
+ }
+
+void clear_screen(){
+	rectfill(screen,0,0,640,480,15);
+}
